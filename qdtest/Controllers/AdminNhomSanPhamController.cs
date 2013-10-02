e00cacd8a52e2eb8b2a7224a5199f050328e178c
quocdunginfo <quocdunginfo@gmail.com>
2013-10-02 22:30:39 +0700

    Làm tới module quản lý Nhóm Sản phẩm
    Template đã có sẵn
    Thêm
    Chỉnh lại phần Xóa, transfer và Sửa

---
+++ qdtest/Controllers/AdminNhomSanPhamController.cs
@@ -0,0 +1,73 @@
+﻿using qdtest._Library;
+using qdtest.Controllers.ModelController;
+using qdtest.Models;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace qdtest.Controllers
+{
+    public class AdminNhomSanPhamController : AdminController
+    {
+        //
+        // GET: /AdminNhomSanPham/
+        public ActionResult Index()
+        {
+            NhomSanPham root = this._db.ds_nhomsanpham.Where(x => x.id == 1).FirstOrDefault();
+            ViewBag.NhomSanPham_List = this._db.ds_nhomsanpham.ToList();
+            ViewBag.Title += " - View";
+            return View();
+        }
+        [HttpPost]
+        public ActionResult Submit()
+        {
+            //get search
+            return View();
+        }
+        [HttpPost]
+        public ActionResult Add()
+        {
+            //get data
+            int p_id = TextLibrary.ToInt(Request["cat_parent_id"]);
+            //nên lấy instance của chính Controller đó để dùng sẽ không bị lỗi track changes
+            NhomSanPhamController controller = new NhomSanPhamController();
+
+            NhomSanPham obj = new NhomSanPham();
+            obj.ten = Request["cat_name"];
+            if (obj.ten.Equals(""))
+            {
+                //lỗi
+                Debug.WriteLine("Lỗi cat_name= rỗng");
+                return RedirectToAction("Index", "AdminNhomSanPham");
+            }
+            NhomSanPham p_obj = controller._db.ds_nhomsanpham.Where(x => x.id == p_id).FirstOrDefault();
+            if (p_obj == null)
+            {
+                //lỗi
+                Debug.WriteLine("Lỗi cat_parent_obj= null (id=)"+p_id);
+                return RedirectToAction("Index","AdminNhomSanPham");
+            }
+            obj.nhomcha = p_obj;
+            //call add
+            controller.add(obj);
+            Debug.WriteLine("Thêm Thành Công Nhóm Sản Phẩm");
+            return RedirectToAction("Index","AdminNhomSanPham");
+        }
+        public ActionResult Delete(int id=0)
+        {
+            NhomSanPhamController controller = new NhomSanPhamController();
+            if (!controller.is_exist(id))
+            {
+                Debug.WriteLine("Delete Nhóm sản phẩm id không tồn tại");
+                return RedirectToAction("Index", "AdminNhomSanPham");
+            }
+            controller.delete(id);
+            Debug.WriteLine("Delete Nhóm sản phẩm thành công (xóa luôn con)");
+            return RedirectToAction("Index", "AdminNhomSanPham");
+        }
+
+    }
+}
---
+++ qdtest/Controllers/ModelController/NhomSanPhamController.cs
@@ -0,0 +1,81 @@
+﻿using qdtest._Library;
+using qdtest.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace qdtest.Controllers.ModelController
+{
+    public class NhomSanPhamController : Controller
+    {
+        public BanGiayDBContext _db = new BanGiayDBContext();
+       
+        public NhomSanPham get_by_id(int id)
+        {
+            return _db.ds_nhomsanpham.FirstOrDefault(x => x.id == id);
+        }
+        public Boolean is_exist(int id)
+        {
+            NhomSanPham u = (from user in _db.ds_nhomsanpham
+                          where user.id == id
+                          select user).FirstOrDefault();
+            return u == null ? false : true;
+        }
+        public int add(NhomSanPham obj)
+        {
+            this._db.ds_nhomsanpham.Add(obj);
+            this._db.SaveChanges();
+            //return ma moi nhat
+            return this._db.ds_nhomsanpham.Max(x => x.id);
+        }
+        public Boolean delete(int id)
+        {
+            //Xóa object có dính khóa ngoại trước
+            NhomSanPham obj = this._db.ds_nhomsanpham.Where(x => x.id == id).FirstOrDefault();
+            if (obj == null) return false;
+            this._db.ds_nhomsanpham.Remove(obj);
+            this._db.SaveChanges();
+            return true;
+        }
+        public Boolean delete(int id, int transfer_id)
+        {
+            //tranfer all object has this ...
+            NhomSanPham transfer = this._db.ds_nhomsanpham.Where(x => x.id == transfer_id).FirstOrDefault();
+            NhomSanPham canxoa = this._db.ds_nhomsanpham.Where(x => x.id == id).FirstOrDefault();
+            if(transfer==null || canxoa==null)
+            {
+                return false;
+            }
+            canxoa.ds_sanpham.ForEach(x => x.nhomsanpham = transfer);
+            //remove
+            this._db.ds_nhomsanpham.Remove(canxoa);
+            this._db.SaveChanges();
+            return true;
+        }
+        public List<NhomSanPham> timkiem(String id = "", String ten = "")
+        {
+            List<NhomSanPham> list = new List<NhomSanPham>();
+            if (!id.Equals(""))
+            {
+                //find by id
+                int id_i = TextLibrary.ToInt(id);
+                list = this._db.ds_nhomsanpham.Where(x => x.id == id_i).ToList();
+                if (list == null)
+                {
+                    list = new List<NhomSanPham>();
+                }
+                return list;
+            }
+            //find by LIKE elament
+            list = this._db.ds_nhomsanpham.Where(x => x.ten.Contains(ten)).ToList();
+            if (list == null)
+            {
+                list = new List<NhomSanPham>();
+            }
+
+            return list;
+        }
+    }
+}
