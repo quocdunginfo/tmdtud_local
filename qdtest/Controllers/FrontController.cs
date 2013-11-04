04402763fa0e59b2c1e400dbe2ffb600833abb9e
kienkimkhung <kienkimkhung@gmail.com>
2013-11-04 10:19:23 +0700

    load sp mới lên front
    dewád

--- qdtest/Controllers/FrontController.cs
+++ qdtest/Controllers/FrontController.cs
@@ -1,5 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Diagnostics;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
@@ -16,14 +18,29 @@ namespace CuaHangBanGiay.Controllers.View_Controller
         protected override void OnActionExecuting(ActionExecutingContext filterContext)
         {
             NhomSanPhamController ctr = new NhomSanPhamController();
-            //ViewBag.NhomSanPham2_List = ctr.timkiem(
-            //    ctr.timkiem_nhomsanpham["id"],
-            //    this.timkiem_nhomsanpham["ten"],
-            //    this.timkiem_nhomsanpham["mota"],
-            //    this.timkiem_nhomsanpham["active"]);
             ViewBag.NhomSanPham2_List_All = ctr.timkiem("", "", "", "");
-            //ViewBag.Title += " - View";
-            //ViewBag.timkiem_nhomsanpham = this.timkiem_nhomsanpham;
+           // var list = ctr._db.Database.SqlQuery<int>("drop table table2 select ChiTietSPs.sanpham_id,sum(ChiTiet_DonHang.soluong) as sl into table2 from ChiTiet_DonHang inner join ChiTietSPs on ChiTiet_DonHang.chitietsp_id=ChiTietSPs.id group by ChiTietSPs.sanpham_id order by sl DESC select sanpham_id from table2 order by sl  DESC").ToList();
+            var list = (from p in ctr._db.ds_sanpham
+                       join c in
+                           ((from ChiTiet_DonHang in ctr._db.ds_chitiet_donhang
+                             group new { ChiTiet_DonHang.chitietsp, ChiTiet_DonHang } by new
+                             {
+                                 Sanpham_id = (int?)ChiTiet_DonHang.chitietsp.sanpham.id,
+                             } into g
+                             select new
+                             {
+                                 Sanpham_id = (int?)g.Key.Sanpham_id,
+                                 sl = (int?)g.Sum(p => p.ChiTiet_DonHang.soluong),
+                             }).OrderByDescending(x => x.sl)) on p.id equals c.Sanpham_id
+                       select new
+                       {
+                           sp=p,
+                           sl=c.sl
+                       }).OrderByDescending(x=>x.sl);
+            foreach (var item in list)
+            {
+                Debug.WriteLine("Kim =" + item.sp.id + "//loai="+item.sp.nhomsanpham.ten+"//sl="+item.sl);
+            }
         }
 
     }
---
+++ qdtest/Controllers/FrontHomeController.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using qdtest.Controllers.ModelController;
+using qdtest.Models;
+
+namespace CuaHangBanGiay.Controllers.View_Controller
+{
+    public class FrontHomeController : FrontController
+    {
+        //
+        // GET: /Default/
+
+        public ActionResult Index()
+        {
+            ViewBag.SanPhamNew_List = spnew();
+            return View();
+        }
+        public List<SanPham> spnew()
+        {
+            SanPhamController ctrl = new SanPhamController();
+            List<SanPham> listnew = ctrl.timkiem("", "", "", "", -1, -1, null, null, "1", "id", true, 0, 6);
+            if (listnew != null)
+            {
+                foreach (var item in listnew)
+                {
+                    item.mota = sub(item.mota);    
+                }
+                return listnew;
+            }
+            return new List<SanPham>();
+              
+        }
+        private String sub(String s)
+        {
+            if (s.Equals("")) return s;
+            //xóa khoảng trắng
+              s = s.Trim();
+            while (s.IndexOf("  ") >= 0)
+               s= s.Replace("  "," ");
+            int space = 0;
+            for (int i = 0; i < s.Length; i++)
+            {
+                if (s[i]==32) space++;
+                if (i == s.Length - 1 && space < 15)
+                {
+                    if(!s.Substring(i-2,3).Equals("..."))
+                    s += "...";
+                    while (space != 15)
+                    {
+                        s += "      ";
+                        space++;
+                    }
+                    return s;
+                }
+                if (space == 15) 
+                { 
+                    s = s.Substring(0,i); 
+                    break; 
+                }
+            }
+            if (!s.Substring(s.Length - 3, 3).Equals("..."))
+                s += "...";
+            return s;
+        }
+    }
+}
