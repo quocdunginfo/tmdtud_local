92afbef22d4cd8586778fb9e7b4ee76f2a327f15
kienkimkhung <kienkimkhung@gmail.com>
2013-11-06 11:32:16 +0700

    Danglam trang tim kiem...
    tim kiem co ban theo ten san pham
    Co phan trang nhung chua dep (can nhac su dung thu vien ngoai)
    Them ham substring trong textlibrary

--- qdtest/Controllers/FrontController.cs
+++ qdtest/Controllers/FrontController.cs
@@ -5,6 +5,7 @@ using System.Diagnostics;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using qdtest._Library;
 using qdtest.Controllers.ModelController;
 using qdtest.Models;
 
@@ -12,9 +13,13 @@ namespace qdtest.Controllers
 {
     public class FrontController : Controller
     {
+        public FrontController()
+        {
+            this._khoitao_cookie();
+        }
         //
         // GET: /Layout/
-
+        protected HttpCookie _timkiem_sanpham;
         protected override void OnActionExecuting(ActionExecutingContext filterContext)
         {
             NhomSanPhamController ctr = new NhomSanPhamController();
@@ -31,6 +36,41 @@ namespace qdtest.Controllers
                 ViewBag.NhomSanPham2_List_All = new List<NhomSanPham2>();
                 ViewBag.SanPham_BestSeller = new List<SanPham>();
             }
+            //tim kiem
+            //build timkiem_nhanvien
+            if (Request.Cookies.Get("front_timkiem_sanpham") == null)
+            {
+                //chưa set cookies trước => tiến hành set cookies
+                this._khoitao_cookie();
+                Response.Cookies.Add(CookieLibrary.Base64Encode(this._timkiem_sanpham));
+            }
+            else
+            {
+                try
+                {
+                    this._timkiem_sanpham = CookieLibrary.Base64Decode(Request.Cookies.Get("timkiem_sanpham"));
+                }
+                catch (Exception ex)
+                {
+                    this._khoitao_cookie();
+                    Response.Cookies.Add(CookieLibrary.Base64Encode(this._timkiem_sanpham));
+                }
+            }
+            ViewBag.timkiem_sanpham = this._timkiem_sanpham;
+        }
+        [NonAction]
+        protected void _khoitao_cookie()
+        {
+            _timkiem_sanpham = new HttpCookie("front_timkiem_sanpham");
+            _timkiem_sanpham.Expires = DateTime.Now.AddDays(1);
+            this._timkiem_sanpham["ten"] = "";
+            this._timkiem_sanpham["masp"] = "";
+            this._timkiem_sanpham["mota"] = "";
+            this._timkiem_sanpham["gia_from"] = "-1";
+            this._timkiem_sanpham["gia_to"] = "-1";
+            this._timkiem_sanpham["hangsx_ten"] = "";
+            this._timkiem_sanpham["nhomsanpham_ten"] = "";
+            this._timkiem_sanpham["max_item_per_page"] = "9";
         }
 
     }
--- qdtest/_Library/TextLibrary.cs
+++ qdtest/_Library/TextLibrary.cs
@@ -53,5 +53,17 @@ namespace qdtest._Library
             }
             return re;
         }
+        public static string Unicode_Substring(string text, int length)
+        {
+            var bytes = Encoding.UTF8.GetBytes(text);
+            var result = Encoding.UTF8.GetString(bytes, 0, Math.Min(bytes.Length, length));
+
+            while ('\uFFFD' == result[result.Length - 1])
+            {
+                result = result.Substring(0, result.Length - 1);
+            }
+
+            return result;
+        }
     }
 }
