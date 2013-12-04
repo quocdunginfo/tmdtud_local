ef751ca3aa518f4425bbc7a973b06046f0a5294b
kienkimkhung <kienkimkhung@gmail.com>
2013-12-04 16:54:53 +0700

    trang giới thiệu

---
+++ qdtest/Controllers/FrontAboutController.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace qdtest.Controllers
+{
+    public class FrontAboutController : FrontController
+    {
+        //
+        // GET: /FrontAbout/
+
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+    }
+}
